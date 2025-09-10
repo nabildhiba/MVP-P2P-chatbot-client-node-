bf06d47d9354e1984e3dcdb5ff739d729c38a183
nabildhiba <99333328+nabildhiba@users.noreply.github.com>
2025-09-11 01:14:26 +0200

    chore: add example config

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,8 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const initialSupply = ethers.parseEther("1000");
   const Token = await ethers.getContractFactory("P2PChatbotToken");
-  const token = await Token.deploy(initialSupply);
+  const token = await Token.deploy();
   await token.waitForDeployment();
   console.log(`P2PChatbotToken deployed to: ${token.target}`);
 }

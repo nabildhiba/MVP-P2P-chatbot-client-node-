d38cd4e7867bd5fadc18f1ba453d7edc33ad50ec
nabildhiba <99333328+nabildhiba@users.noreply.github.com>
2025-09-11 01:06:18 +0200

    chore: update hardhat network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ const { POLYGON_RPC_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    mumbai: {
+    polygonMumbai: {
       url: POLYGON_RPC_URL || "",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
